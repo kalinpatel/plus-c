35d6f895d975dfdf3cb208d81ee292604d22b09e
Kalin Patel <52113248+kalinpatel@users.noreply.github.com>
2022-05-28 20:53:53 -0500

    Message to comply with Google trust verification

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,21 @@ import Layout from "@/templates/layout";
 export default function Home() {
   return (
     <Layout title="Home">
-      <Header title="Home" subtitle="No content yet" />
+      <Header title="Home" subtitle="No visual content yet" />
+      <p>
+        This app is meant to calculate the answer to a given calculus problem.
+        It is able to run fully offline when installed as a PWA, and can
+        generate solutions on your device, without needing to reach out to a
+        server.
+      </p>
+      <p>
+        Data is collected through Google Analytics and Firebase Performance
+        Monitoring when you use this site. When you sign in, your account
+        information is saved. When you perform a calculation while logged in,
+        your calculation is saved, and is linked to you. See the{" "}
+        <a href="/legal/privacy">privacy policy</a> for more details and for
+        instructions on deleting your account.
+      </p>
       <OneTapLogin />
     </Layout>
   );
